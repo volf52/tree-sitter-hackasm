1e722ee220e9be04cc1992a2ea2107f120f15e12
Arslan <rslnkrmt2552@gmail.com>
2023-08-07 17:20:48 +0500

    update grammar to ensure newlines after every inst

--- grammar.js
+++ grammar.js
@@ -7,13 +7,16 @@ const DEST_CHOICES = buildPermutations(REGISTERS.join(''));
 
 module.exports = grammar({
   name: 'hackasm',
+  extras: ($) => [' '],
   rules: {
     source_file: ($) => repeat($._line),
     _line: ($) => choice($.comment, $.label_def, $._instruction),
+    _newline: ($) => /\r?\n|\r/,
     label_def: ($) => seq('(', $.label_ident, ')'),
     _instruction: ($) => choice($.a_inst, $.c_inst),
-    a_inst: ($) => seq('@', $.label_ident),
-    c_inst: ($) => seq(optional($._dest_def), $.comp, optional($._jump_def)),
+    a_inst: ($) => seq('@', $.label_ident, $._newline),
+    c_inst: ($) =>
+      seq(optional($._dest_def), $.comp, optional($._jump_def), $._newline),
     label_ident: ($) => choice($._alpha, $._numeric),
     _dest_def: ($) => seq($.dest, '='),
     _jump_def: ($) => seq(';', $.jump),
